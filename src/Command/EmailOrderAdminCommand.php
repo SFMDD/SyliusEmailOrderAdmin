6e484716c6ee3ed2b0023651eb49e4574ccda6bf
Reyo Stallenberg <reyostallenberg@gmail.com>
2020-05-26 21:58:09 +0200

    Make it possible to translate e-mail and add en and nl translations

--- src/Command/EmailOrderAdminCommand.php
+++ src/Command/EmailOrderAdminCommand.php
@@ -14,10 +14,12 @@ class EmailOrderAdminCommand extends Command {
      * @var Sender
      */
     private $sender;
+
     /**
      * @var OrderRepository
      */
     private $repositoryOrder;
+
     private $emailsAdmin;
 
     public function __construct(Sender $sender, OrderRepository $orderRepository, array $emailsAdmin)
@@ -25,23 +27,26 @@ class EmailOrderAdminCommand extends Command {
         $this->sender = $sender;
         $this->repositoryOrder = $orderRepository;
         $this->emailsAdmin = $emailsAdmin;
+
         parent::__construct();
     }
 
     protected function configure(): void {
         $this
             ->setName('fmdd:email:order:admin')
-            ->setDescription('');
+            ->setDescription('Test send an admin order e-mail');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) : void {
         $order = $this->repositoryOrder->createQueryBuilder('p')->setMaxResults(1)->getQuery()->getOneOrNullResult();
 
-        if(!is_null($order))
-            $this->sender->send('order_payed',
-                $this->emailsAdmin, ['order' => $order]);
-        else
-            $output->writeln("Erreur you need to have one order");
-        $output->writeln("Finish execution, list of email admin : ". implode(",", $this->emailsAdmin));
+        if (!is_null($order)) {
+            $this->sender->send('order_payed', $this->emailsAdmin, ['order' => $order]);
+        }
+        else {
+            $output->writeln('<error>You need to have at least one order in the database.</error>');
+        }
+
+        $output->writeln(sprintf('Finished execution, list of email admin: %s', implode(',', $this->emailsAdmin)));
     }
 }
